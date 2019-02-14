586d8a5d5e25254aa90714c7885015576adc484f
Renato de Leão <renatodeleao@gmail.com>
2019-02-14 13:13:50 +0000

    updated deploy script

--- docs-deploy.sh
+++ docs-deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:renatodeleao/a11y-vue-dialog.git master:gh-pages
+ git push -f $1 master:gh-pages
 
 cd -
