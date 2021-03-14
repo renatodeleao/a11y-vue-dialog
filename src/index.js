89a9b83a31d2a4e7ad5909a5ea6a044e2855df72
Renato de Leão <renatodeleao@gmail.com>
2021-03-14 16:28:44 +0000

    refactor!: remove styled version of A11yVueDialog
    - first it was complete out of sync with the main implementation
    - secondly is not the focus of this package, there are a myriad of styled implementations so we recommend using those if that's what you want. This is tailored to devs who want full controll.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import A11yVueDialog from "./A11yVueDialog.vue";
 import A11yVueDialogRenderless from "./A11yVueDialogRenderless.vue";
-// optional stylesheet
-import "./styles/a11y-vue-dialog.scss";
 
 const DEFAULTS = {
   componentName: "a11y-vue-dialog",
@@ -22,16 +19,16 @@ var Plugin = {
     // is there a better way of doing this
     // (extend default prop value on component registration)*
     if( options.props) {
-      Object.keys(A11yVueDialog.props).map(propName => {
+      Object.keys(A11yVueDialogRenderless.props).map(propName => {
         if(options.props[propName]) {
-          A11yVueDialog.props[propName]['default'] = options.props[propName];
+          A11yVueDialogRenderless.props[propName]['default'] = options.props[propName];
         }
       });
     }
 
-    Vue.component(this.componentName, A11yVueDialog)
+    Vue.component(this.componentName, A11yVueDialogRenderless)
   }
 }
 
-export { A11yVueDialog, A11yVueDialogRenderless };
+export { A11yVueDialogRenderless };
 export default Plugin;
